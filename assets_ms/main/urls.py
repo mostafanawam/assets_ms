f4b38e8f24cea2ff16f663a10e91f68f377683ae
Mostafa Nawam <mynawam@bluemint.ai>
2024-10-21 20:30:45 +0300

    update the README file

--- assets_ms/main/urls.py
+++ assets_ms/main/urls.py
@@ -5,11 +5,11 @@ app_name = "main"
 urlpatterns = [
       
 
-      path('', asset_view, name='asset_view'),
-      path('asset/<int:asset_id>/',edit_asset, name='edit_asset'),
-      path('assets/delete/<int:asset_id>/', delete_asset, name='delete_asset'),
-      path('asset/lend/<int:asset_id>/',lend_asset, name='lend_asset'),
-      path('asset/return/<int:asset_id>/',return_asset, name='return_asset'),
+      path('', asset_view, name='asset_view'), # assets list/create
+      path('asset/<int:asset_id>/',edit_asset, name='edit_asset'), # edit asset
+      path('assets/delete/<int:asset_id>/', delete_asset, name='delete_asset'),  # delete asset
+      path('asset/lend/<int:asset_id>/',lend_asset, name='lend_asset'), # lend asset
+      path('asset/return/<int:asset_id>/',return_asset, name='return_asset'), # return asset
       
       path('login/', login_view, name='login_view'),
       path('logout/', logout_view, name='logout_view'),
--- assets_ms/main/views.py
+++ assets_ms/main/views.py
@@ -15,7 +15,7 @@ def login_view(request):
 
         if user is not None:
             login(request, user)
-            return redirect('main:asset_view')  # Redirect to a success page, replace 'home' with your URL name
+            return redirect('main:asset_view')  # Redirect to the dashboard
         else:
             messages.error(request, 'Invalid username or password.')
 
@@ -23,15 +23,15 @@ def login_view(request):
 
 def logout_view(request):
     logout(request)  # Log the user out
-    return redirect('main:login_view')  # Redirect to the login page (or any page you prefer)
+    return redirect('main:login_view')  # Redirect to the login page
 
-@login_required(login_url=reverse_lazy('main:login_view'))
+@login_required(login_url=reverse_lazy('main:login_view')) # accessing this route need to be logged in else redirected to login
 def asset_view(request):
     if request.method == 'POST':
         # Handle asset creation
         form = AssetForm(request.POST)
-        if form.is_valid():
-            form.save()
+        if form.is_valid(): # validate the form
+            form.save() # create the asset
             return redirect('main:asset_view')  # Redirect back to the asset list after saving
     else:
         # Handle GET: Display the list of assets
@@ -40,16 +40,16 @@ def asset_view(request):
         context={
             'assets': assets, 
             'form': form,
-            "employees":Employee.objects.all()
+            "employees":Employee.objects.all() # list of employees used in lending
         }
         return render(request, 'asset_list.html',context=context )
 
 @login_required(login_url=reverse_lazy('main:login_view'))
 def delete_asset(request, asset_id):
-    asset = get_object_or_404(Asset, id=asset_id)
+    asset = get_object_or_404(Asset, id=asset_id) # check if exist else 404
     if request.method == 'POST':
-        asset.delete()
-        return redirect('main:asset_view')
+        asset.delete() # delete asset
+        return redirect('main:asset_view') # Redirect back to the asset list after deleting
     
 @login_required(login_url=reverse_lazy('main:login_view'))
 def edit_asset(request, asset_id):
@@ -69,11 +69,11 @@ def lend_asset(request,asset_id):
     
     if request.method == 'POST':
         employee = get_object_or_404(Employee, id=request.POST['employee'])
-        Lending.objects.create(
+        Lending.objects.create( # create lending instance
             asset=asset,
-            employee=employee
+            employee=employee   # assign the selected employee as the lender
         )
-        asset.status="lent"
+        asset.status="lent" # change the status to lent(not available )
         asset.save()
         return redirect('main:asset_view')
 
@@ -82,10 +82,10 @@ def lend_asset(request,asset_id):
 def return_asset(request,asset_id):
     asset = get_object_or_404(Asset, id=asset_id)
     if request.method == 'POST':
-        asset.status="available"
+        asset.status="available" # update the status of the asset to available    
         asset.save()
-        current_lending=asset.current_lending
-        current_lending.return_date=timezone.now()
+        current_lending=asset.current_lending   # get the last active lending process
+        current_lending.return_date=timezone.now()  # assign the lending returning date
         current_lending.save()
         return redirect('main:asset_view')
         
